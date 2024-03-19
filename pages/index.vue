9f3a12fe0db56f8b5d606987c88523e5e2ca8d6b
SuperMilesBros <66653712+supermilesbros@users.noreply.github.com>
2024-03-19 19:23:34 -0400

    more changes

--- pages/index.vue
+++ pages/index.vue
@@ -41,7 +41,7 @@
           label="Time"
           :items="['11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm']"
         ></v-select>
-        <v-btn @click="submit" color="primary" block>Submit</v-btn>
+        <v-btn @click="submit" color="light-blue-lighten-2" block>Submit</v-btn>
       </v-form>
     </v-card>
   </template>
--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -1,8 +1,8 @@
 <template >
-    <div>
-        <v-card class="TYcard" elevated>
-            <v-card-title>Thank You for Scheduling</v-card-title>
-            <v-card-subtitle>You will receve a confirmation email with the details of your appointment.</v-card-subtitle>
+    <div class="TYcard-container">
+        <v-card class="TYcard rounded-lg" elevation="16" elevated>
+            <v-card-title class="TYcard-title">Thank You for Scheduling</v-card-title>
+            <p class="TYcard-subtitle">You will receve a confirmation email with the details of your appointment.</p>
             <v-card-text>
                 <h5 class="mt-5 mapTagTitle">Grand Park</h5>
                 <a class="mapTag" href="https://maps.app.goo.gl/SYcqJeE3YXHXdDqq8" target="_blank">200 E 186th St A, Westfield, IN 46074</a>
@@ -11,18 +11,45 @@
             </v-card-text>
         </v-card>
     </div>
+    <div class="learnBTN">
+        <v-btn href='https://www.recoveryroomusa.com' target="_blank" color="light-blue-lighten-2" block>Learn More</v-btn>
+    </div>
 </template>
 <script setup>
-// route to thank you page
 
 
 
 
 </script>
 <style lang="scss">
-.TYcard {
+.brandColor {
+    color: #00ccff;
+}
+.TYcard-container {
     padding: 1rem;
-    margin: 1rem;
+}
+.TYcard {
     text-align: center;
+    
+}
+.TYcard-title {
+    font-size: 1.5rem;
+    font-weight: 500;
+}
+.TYcard-subtitle {
+    font-size: .9rem;
+    font-weight: 400;
+}
+.mapTagTitle {
+  color: white;
+  font-size: 1.2rem;
+  text-decoration: none;
+}
+.mapTag {
+  color: #00ccff;
+  text-decoration: underline;
+}
+.learnBTN {
+    padding: 1rem;
 }
 </style>
