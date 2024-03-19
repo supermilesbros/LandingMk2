936274052a081645299bdb913d9585ec3751bf29
SuperMilesBros <66653712+supermilesbros@users.noreply.github.com>
2024-03-19 18:57:17 -0400

    changes to thank you page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-stepper class="stepper-container" :items="['Personal Info', 'Visit Date', 'Thank You']" mobile>
+  <v-stepper class="stepper-container" :items="['Personal Info', 'Visit Date']" mobile>
   <template v-slot:item.1>
     <v-card class="h-100" title="Personal Info" flat>
       <v-form @submit.prevent="submit">
@@ -41,12 +41,12 @@
           label="Time"
           :items="['11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm']"
         ></v-select>
-        <v-btn type="submit" color="primary">Submit</v-btn>
+        <v-btn @click="submit" color="primary" block>Submit</v-btn>
       </v-form>
     </v-card>
   </template>
 
-  <template v-slot:item.3>
+  <!-- <template v-slot:item.3>
     <v-card title="Thank You for Scheduling" flat>
       <p>You will receve a confirmation email with the details of your appointment.</p>
       <h5 class="mt-5 mapTagTitle">Grand Park</h5>
@@ -55,21 +55,26 @@
       <a class="mapTag" href="https://maps.app.goo.gl/6vGgZuTXhi8pn5XP9" target="_blank">20 Executive Dr suite g, Carmel, IN 46032</a>
       
     </v-card>
-  </template>
+  </template> -->
 </v-stepper>
 </template>
 
 <script setup>
+const router = useRouter();
+
 const email = ref("");
 const password = ref("");
 const name = ref("");
 const phone = ref("");
 
+const submit = () => {
+  console.log("submit");
+    router.push("/thank-you");
+}
+
 
 
 const { ruleEmail, rulePassLen, ruleRequired } = useFormRules();
-
-const submit = async () => {};
 </script>
 <style lang="scss">
 .stepper-container {
---
+++ pages/thank-you.vue
@@ -0,0 +1,28 @@
+<template >
+    <div>
+        <v-card class="TYcard" elevated>
+            <v-card-title>Thank You for Scheduling</v-card-title>
+            <v-card-subtitle>You will receve a confirmation email with the details of your appointment.</v-card-subtitle>
+            <v-card-text>
+                <h5 class="mt-5 mapTagTitle">Grand Park</h5>
+                <a class="mapTag" href="https://maps.app.goo.gl/SYcqJeE3YXHXdDqq8" target="_blank">200 E 186th St A, Westfield, IN 46074</a>
+                <h5 class="mt-5 mapTagTitle">Carmel</h5>
+                <a class="mapTag" href="https://maps.app.goo.gl/6vGgZuTXhi8pn5XP9" target="_blank">20 Executive Dr suite G, Carmel, IN 46032</a>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+<script setup>
+// route to thank you page
+
+
+
+
+</script>
+<style lang="scss">
+.TYcard {
+    padding: 1rem;
+    margin: 1rem;
+    text-align: center;
+}
+</style>
