aad45c8cf0c5178bd9b411f405d99c1a81afafd2
SuperMilesBros <66653712+supermilesbros@users.noreply.github.com>
2024-03-19 20:41:34 -0400

    sdff

--- .nuxt/pwa/sw.js
+++ .nuxt/pwa/sw.js
@@ -1,4 +1,4 @@
-const options = {"autoRegister":true,"cacheOptions":{"directoryIndex":"/","revision":"WMCUfoztU7tS"},"enabled":true,"preCaching":[],"templatePath":null,"workboxVersion":"6.5.3","workboxUrl":"https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js"}
+const options = {"autoRegister":true,"cacheOptions":{"directoryIndex":"/","revision":"yghq3IquEmij"},"enabled":true,"preCaching":[],"templatePath":null,"workboxVersion":"6.5.3","workboxUrl":"https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js"}
 
 importScripts(options.workboxUrl)
 
